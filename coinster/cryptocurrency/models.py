c6fc0adbafb8d5a897c4ad4152c281b1cd9b550d
sahil-2118 <78615683+sahil-2118@users.noreply.github.com>
2023-07-23 13:00:05 -0700

    add cryptocurrency model

--- coinster/cryptocurrency/models.py
+++ coinster/cryptocurrency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class CryptoCurrency(models.Model):
+
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=50)
+    rank = models.IntegerField()
+    circulating_supply = models.BigIntegerField()
+    total_supply = models.BigIntegerField()
+    volume_24h = models.IntegerField()
+    percent_change_24h = models.FloatField()
+    percent_change_1h = models.FloatField()
+    percent_change_7d = models.FloatField()
+    market_cap = models.FloatField()
+    last_update = models.DateTimeField()
