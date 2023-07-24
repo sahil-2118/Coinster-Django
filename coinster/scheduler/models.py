3e40e4a48c5e282151a053b64bc192153374ffc8
sahil-2118 <78615683+sahil-2118@users.noreply.github.com>
2023-07-24 01:32:33 -0700

    add scheduler model

--- coinster/scheduler/models.py
+++ coinster/scheduler/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from ..cryptocurrency.models import CryptoCurrency
 
-# Create your models here.
+User = get_user_model()
+
+class Scheduler(models.Model):
+
+    time_range = models.BigIntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    crypto = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    deleted_at = models.DateTimeField()
+
