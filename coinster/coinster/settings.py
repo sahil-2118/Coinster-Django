bcf9af763a3db5913bd1adfe0107958241d4191c
sahil-2118 <78615683+sahil-2118@users.noreply.github.com>
2023-07-24 03:58:01 -0700

    make migrations

--- coinster/coinster/settings.py
+++ coinster/coinster/settings.py
@@ -114,7 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-#AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'user.User'
 
 
 # Static files (CSS, JavaScript, Images)
--- coinster/scheduler/models.py
+++ coinster/scheduler/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from ..cryptocurrency.models import CryptoCurrency
+from cryptocurrency.models import CryptoCurrency
 
 User = get_user_model()
 
