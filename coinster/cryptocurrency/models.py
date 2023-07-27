d1efba90ec572085afdcf93f68a1f62a0f87b1d0
sahil-2118 <78615683+sahil-2118@users.noreply.github.com>
2023-07-27 11:59:22 -0700

    add Meta class to crypto model

--- coinster/cryptocurrency/models.py
+++ coinster/cryptocurrency/models.py
@@ -2,16 +2,58 @@ from django.db import models
 
 class CryptoCurrency(models.Model):
 
-    name = models.CharField(max_length=50)
-    symbol = models.CharField(max_length=50)
-    rank = models.IntegerField()
-    circulating_supply = models.BigIntegerField()
-    total_supply = models.BigIntegerField()
-    volume_24h = models.IntegerField()
-    percent_change_24h = models.FloatField()
-    percent_change_1h = models.FloatField()
-    percent_change_7d = models.FloatField()
-    market_cap = models.FloatField()
-    last_update = models.DateTimeField()
-    created_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
+    name                = models.CharField(
+                                            max_length=50,
+                                        )
+    symbol              = models.CharField(
+                                            max_length=50,
+                                        )
+    rank                = models.PositiveSmallIntegerField()
+    circulating_supply  = models.BigIntegerField(
+                                            null=True,
+                                            blank=True,
+                                            )
+    total_supply        = models.BigIntegerField(
+                                            null=True,
+                                            blank=True,
+                                            )
+    volume_24h          = models.IntegerField()
+    percent_change_24h  = models.FloatField(
+                                            null=True,
+                                            blank=True,
+                                            )
+    percent_change_1h   = models.FloatField(
+                                            null=True,
+                                            blank=True,
+                                            )
+    percent_change_7d   = models.FloatField(
+                                            null=True,
+                                            blank=True,
+                                            )
+    market_cap          = models.FloatField()
+    last_update         = models.DateTimeField(
+                                            auto_now=True,
+                                            )
+    created_at          = models.DateTimeField(
+                                            auto_now_add=True,
+                                            )
+    deleted_at          = models.DateTimeField(
+                                            null=True,
+                                            )
+
+    class Meta:
+        ordering = ["rank"]
+        verbose_name_plural = "cryptocurrencies"
+        indexes = [
+            models.Index(fields=["rank",]),
+            models.Index(fields=["symbol",]),
+            models.Index(fields=["created_at",]),
+            ]
+    
+
+
+
+
+
+
+
