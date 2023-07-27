a60443055fbfa2a0bc4e01987b0b3475e6a3fee2
sahil-2118 <78615683+sahil-2118@users.noreply.github.com>
2023-07-27 11:58:23 -0700

    add Meta class to scheduler model

--- coinster/scheduler/models.py
+++ coinster/scheduler/models.py
@@ -1,15 +1,46 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+
 from cryptocurrency.models import CryptoCurrency
 
 User = get_user_model()
 
 class Scheduler(models.Model):
 
-    time_range = models.BigIntegerField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    crypto = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
+    time_range  = models.BigIntegerField(
+                                verbose_name=_("""
+                                               this is time range for store the range of time that user want to send data
+                                               """),
+                                )
+    owner       = models.ForeignKey(
+                                to = User,
+                                verbose_name=_("the owner of scheduler"),
+                                related_name="schedulers",
+                                on_delete=models.CASCADE,
+                            )
+    crypto      = models.ForeignKey(
+                                to = CryptoCurrency,
+                                verbose_name=_("the crypto of scheduler"),
+                                related_name="schedulers",
+                                on_delete=models.CASCADE,
+                            )
+    created_at  = models.DateTimeField(
+                                auto_now_add=True,
+                                )
+    updated_at  = models.DateTimeField(
+                                auto_now=True,
+                                )
+    deleted_at  = models.DateTimeField(
+                                null=True,
+                                )
+    class Meta:
+       ordering = ["-created_at"]
+       verbose_name_plural = "schedulers"
+       indexes =[
+           models.Index(fields=["owner",]),
+           models.Index(fields=["created_at",]),
+       ] 
+
+
 
